41c4c122837683f752f813e6680f84dd78d1d835
Garnzzz <154430794+Garnzzz@users.noreply.github.com>
2025-09-06 09:03:01 +1000

    Update keymap.c

--- keyboards/bastardkb/charybdis/4x6/keymaps/garnzzz/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/garnzzz/keymap.c
@@ -30,8 +30,8 @@ enum charybdis_keymap_layers {
 /** \brief Automatically enable sniping-mode on the pointer layer. */
 #define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
 
-#ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-static uint16_t auto_pointer_layer_timer = 0;
+//#ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+//static uint16_t auto_pointer_layer_timer = 0;
 
 //#    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
 //#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
